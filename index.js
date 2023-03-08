a85c3339d9c8e505a254803843f87d12198be5e8
RishabhBajpai97 <bajpai.rishabh1018@gmail.com>
2023-03-08 11:50:10 +0530

    first verison tailwind utility

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+const exec = require("child_process").exec;
+function installTailwind() {
+    console.log("Installing Tailwind......")
+    exec("npm install -D tailwindcss postcss autoprefixer", (e, stdout, stderr) => {
+        if (e instanceof Error) {
+            console.log(e)
+            throw e
+        }
+        exec("npx tailwindcss init -p", (e, stdout, stderr) => {
+            if (e instanceof Error) {
+                console.log(e)
+                throw e
+            }
+            const tailwindConfigContent = fs.readFileSync(`${__dirname}/config.txt`);
+            const globalCssContent = fs.readFileSync(`${__dirname}/global.txt`);
+            fs.writeFileSync(`${process.cwd()}/tailwind.config.js`, tailwindConfigContent)
+            fs.writeFileSync(`${process.cwd()}/global.css`, globalCssContent, { flag: "w+" });
+            console.log("Installation Done. Have Fun")
+        })
+
+    })
+
+}
+exports.tailwindPublic = installTailwind
